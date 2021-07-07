b7eaedf5e982b41d6780582a3c5891407fe42b38
Josias Viveiro <josiasviveiro@gmail.com>
2021-07-07 17:38:19 -0300

    Page - Comments

---
+++ src/pages/Comments.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/Header';
+import { MenuSideBar } from '../components/MenuSideBar';
+
+export default function Comments() {
+  return (
+    <div>
+      <Header title="Comentários"/>
+      <MenuSideBar/>
+    </div>
+  )
+}
