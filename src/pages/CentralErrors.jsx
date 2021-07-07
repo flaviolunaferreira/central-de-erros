7783a864b79903eb6677293f5628b56bfbffd9d5
Josias Viveiro <josiasviveiro@gmail.com>
2021-07-07 18:41:17 -0300

    Page - CentralErros

--- src/pages/CentralErrors.jsx
+++ src/pages/CentralErrors.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { CentralErrorsSideBar } from '../components/CentralErrorsSideBar'
+import Header from '../components/Header'
 
 export default function CentralErrors() {
   return (
     <div>
+      <Header title="Perfil" />
       CentralErrors
+      <CentralErrorsSideBar />
     </div>
   )
 }
