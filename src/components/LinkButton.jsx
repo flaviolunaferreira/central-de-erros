57f1daf1a68bfbb3cf3abaa40ae9fc0efa8f1625
Josias Viveiro <josiasviveiro@gmail.com>
2021-07-07 17:36:10 -0300

    Componente LinkButton

---
+++ src/components/LinkButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+
+export default function LinkButton({title, route, onClose}) {
+  return (
+    <div>
+      <Link as={RouterLink} to={route} variant="outline" mr={3} onClick={onClose}>
+        {title}
+      </Link>
+    </div>
+  )
+}
