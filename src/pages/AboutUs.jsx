ad4851b873ee68a960cfdb947c70eea0cb9f7bd0
Josias Viveiro <josiasviveiro@gmail.com>
2021-07-07 17:37:08 -0300

    Page - AboutUs

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header'
+import { MenuSideBar } from '../components/MenuSideBar'
+
+export default function AboutUs() {
+  return (
+    <div>
+      <Header title="Quem Somos"/>
+      <MenuSideBar/>
+    </div>
+  )
+}
