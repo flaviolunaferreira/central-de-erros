527eb2736d261d8f0c2390ca40b2973b2f1cbd31
Josias Viveiro <josiasviveiro@gmail.com>
2021-07-07 18:42:29 -0300

    Page - Type

---
+++ src/pages/Type.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { CentralErrorsSideBar } from '../components/CentralErrorsSideBar'
+import Header from '../components/Header'
+
+export default function Type() {
+  return (
+    <div>
+      <Header title="Perfil" />
+      Busca por tipo
+      <CentralErrorsSideBar />
+    </div>
+  )
+}
