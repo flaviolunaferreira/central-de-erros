68f9a532753176cca526234ffec498dcafcc040c
Josias Viveiro <josiasviveiro@gmail.com>
2021-07-07 18:41:42 -0300

    Page - Level

---
+++ src/pages/Level.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { CentralErrorsSideBar } from '../components/CentralErrorsSideBar'
+import Header from '../components/Header'
+
+export default function Level() {
+  return (
+    <div>
+      <Header title="Perfil" />
+      Busca por nível ou level
+      <CentralErrorsSideBar />
+    </div>
+  )
+}
