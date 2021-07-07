5d5e312f9fa5b6f49cfe1d1e6aa5ee7ef4a7f770
Josias Viveiro <josiasviveiro@gmail.com>
2021-07-07 17:39:02 -0300

    Index para exportação das páginas

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as Home } from "./Home";
+export { default as AboutProject } from "./AboutProject";
+export { default as AboutUs } from "./AboutUs";
+export { default as Comments } from "./Comments";
+export { default as Login } from "./Login";
+export { default as CentralErrors } from "./CentralErrors";
