ec7fe71a7366346f7c0829ce9397d3e61496d3c1
Josias Viveiro <josiasviveiro@gmail.com>
2021-07-07 18:41:33 -0300

    Page - Custom

---
+++ src/pages/Custom.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { CentralErrorsSideBar } from '../components/CentralErrorsSideBar'
+import Header from '../components/Header'
+
+export default function Custom() {
+  return (
+    <div>
+      <Header title="Perfil" />
+      Busca personalizada ou customizada
+      <CentralErrorsSideBar />
+    </div>
+  )
+}
