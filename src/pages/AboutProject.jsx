c93828b644c694f368d747cd7b074d99cc516e42
Josias Viveiro <josiasviveiro@gmail.com>
2021-07-07 17:37:37 -0300

    Page - AboutProjetct

---
+++ src/pages/AboutProject.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header'
+import { MenuSideBar } from '../components/MenuSideBar'
+
+export default function AboutProject() {
+  return (
+    <div>
+      <Header title="Sobre o Projeto"/>
+      <MenuSideBar/>
+    </div>
+  )
+}
