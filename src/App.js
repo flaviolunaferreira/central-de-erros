9ab9e3653e82d1bb47c8b1262263fa36c306b55b
Josias Viveiro <josiasviveiro@gmail.com>
2021-07-07 18:44:02 -0300

    App.js - implementação das rotas da Central de Erros e suas buscas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { 
-  Home, Comments, AboutUs, AboutProject, Login, CentralErrors
- } from "./pages";
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {
+  Home,
+  Comments,
+  AboutUs,
+  AboutProject,
+  Login,
+  CentralErrors,
+  SearchDate,
+  Type,
+  Origin,
+  Level,
+  Custom,
+  Description,
+} from './pages';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <ChakraProvider>
-          <Route exact path="/" component={Home}/>
+          <Route exact path="/" component={Home} />
           <Route path="/comments" component={Comments} />
           <Route path="/aboutus" component={AboutUs} />
-          <Route path="/aboutproject" component={AboutProject}/>
-          <Route path="/login" component={Login}/>
-          <Route path="/centralerrors" component={CentralErrors}/>
+          <Route path="/aboutproject" component={AboutProject} />
+          <Route path="/login" component={Login} />
+
+          <Route path="/centralerrors" component={CentralErrors} />
+          <Route path="/date" component={SearchDate} />
+          <Route path="/type" component={Type} />
+          <Route path="/origin" component={Origin} />
+          <Route path="/level" component={Level} />
+          <Route path="/description" component={Description} />
+          <Route path="/custom" component={Custom} />
         </ChakraProvider>
       </Switch>
     </BrowserRouter>
