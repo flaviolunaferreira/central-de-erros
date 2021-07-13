f51c8bff73a5ec26c68da3cf684246454857566d
jorgehsosiro <jojimurakabe@gmail.com>
2021-07-13 15:40:47 -0300

    add: comment component

---
+++ src/components/CommentsCard.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {
+  Image,
+  Box,
+  Text,
+  Flex,
+  Center,
+} from "@chakra-ui/react";
+
+const CommentsCard = ({ myImage, text }) => {
+
+  return (
+    <Box>
+      <Center>
+          <Flex direction="row">
+            <Image
+              borderRadius="full"
+              boxSize="200px"
+              src={myImage}
+              alt="Segun Adebayo"
+              mt={10}
+              boxShadow= "5px 5px 5px black"
+            />
+
+            <Box
+              alignSelf="center"
+              borderRadius="3xl"
+              bg="#ED1941"
+              mt={10}
+              ml={5}
+              boxShadow= "5px 5px 5px black"
+            >
+              <Text
+                width="3xl"
+                padding="10"
+                color="white"
+                fontFamily="Montserrat"
+                fontSize={20}
+                overflow="hidden"
+              >
+                {text}
+              </Text>
+            </Box>
+          </Flex>
+      </Center>
+    </Box>
+  );
+};
+
+export default CommentsCard;
