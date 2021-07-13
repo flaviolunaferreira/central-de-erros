abc271f362d4230973eeee789397a9e7da744213
Josias Viveiro <josiasviveiro@gmail.com>
2021-07-13 18:09:36 -0300

    component - AboutPerson - sombra para os avatares

--- src/components/AboutPerson.jsx
+++ src/components/AboutPerson.jsx
@@ -5,8 +5,8 @@ export default function AboutPerson({ name, src, link }) {
   return (
     <Link href={link} isExternal>
       <Flex direction="column" align="center">
-        <Avatar size="2xl" name={name} src={src} />
-        <Text>{name}</Text>
+        <Avatar size="2xl" name={name} src={src} boxShadow="5px 5px 5px black" />
+        <Text paddingTop="10px">{name}</Text>
       </Flex>
     </Link>
   );
