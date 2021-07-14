69d6088b460646f89c3a06a7b027cb8991bd662c
jorgehsosiro <jojimurakabe@gmail.com>
2021-07-13 22:46:14 -0300

    add: front tecnologies

--- src/pages/AboutProject.jsx
+++ src/pages/AboutProject.jsx
@@ -6,13 +6,15 @@ import Spring from '../images/springLogo.svg';
 import Maven from '../images/mavenLogo.svg';
 import Oauth from '../images/oauthLogo.svg';
 import Mysql from '../images/mysqlLogo.svg';
+import ReactLogo from '../images/reactLogo.svg';
+import Chakra from '../images/chakraLogo.svg';
 
 export default function AboutProject() {
   return (
     <div>
       <Header title="Sobre o Projeto" />
       <Center>
-        <Box width="8xl" bg="#0C9FA6" padding="38" mt={22} borderRadius="3xl" boxShadow= "5px 5px 5px black">
+        <Box width="8xl" bg="#0C9FA6" padding="38" mt={22} mb={12} borderRadius="3xl" boxShadow= "12px 12px 15px black">
           <Heading size="xl" color="white">
             Introdução
           </Heading>
@@ -37,12 +39,23 @@ export default function AboutProject() {
           <Heading mt={10} size="xl" color="white">
             Tecnologias
           </Heading>
-          <Flex flexFlow="wrap">
+          <Heading mt={8} ml={5} size="lg" color="white">
+            Back-end
+          </Heading>
+          <Flex flexFlow="wrap" ml={5}>
             <Image width={50} mr={10} src={Java} />
             <Image width={100} mr={10} src={Spring} />
             <Image width={100} mr={10} src={Maven} />
             <Image width={90} mr={10} src={Oauth} />
             <Image width={100} mr={10} src={Mysql} />
+          </Flex>
+
+          <Heading mt={8} ml={5} size="lg" color="white">
+            Front-end
+          </Heading>
+          <Flex flexFlow="wrap" ml={5} mt={5}>
+            <Image width={50} mr={10} src={ReactLogo} />
+            <Image width={100} mr={10} src={Chakra} />
           </Flex>
         </Box>
       </Center>
