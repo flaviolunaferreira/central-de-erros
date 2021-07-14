5ec89bc06d12b87f9fce8df528a6df7cb35dff5a
jorgehsosiro <jojimurakabe@gmail.com>
2021-07-13 23:27:08 -0300

    update: service login off

--- src/service/api.js
+++ src/service/api.js
@@ -4,9 +4,9 @@ const api = axios. create({
     baseURL: "http://localhost:8080",
 });
 
-const loginUser = (email, password) =>
-  api.post("/user", { email, password });
+// const loginUser = (email, password) =>
+//   api.post("/user", { email, password });
 
 const registerUser = (name, email, password) => api.post('/user', {name, email, password});
 
-export default {registerUser, loginUser};
+export default {registerUser};
