ad1c230e828f86156640fd9469e7c06e58b2e956
jorgehsosiro <jojimurakabe@gmail.com>
2021-07-15 18:29:15 -0300

    add: begin login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   FormControl,
   FormLabel,
   Input,
   Button,
-  Container
+  Container,
+  Text
 } from "@chakra-ui/react";
 import { MenuSideBar } from '../components/MenuSideBar';
 import LinkButton from '../components/LinkButton';
+import api from '../service/api';
+import { useHistory } from 'react-router-dom';
 
 export default function Login() {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const history = useHistory();
+  
+  const handleLogin = () => {
+    // if (userName === '' || password === '') {
+    //   setMessage('preencha todos os campos');
+    // } else {
+      // api.loginUser(userName, password).then(()=> 
+      history.push("/centralerrors");
+    // }
+  }
+
   return (
     <div>
       <MenuSideBar />
@@ -17,16 +35,16 @@ export default function Login() {
         <form>
           <FormControl id="user">
             <FormLabel>Usuário</FormLabel>
-            <Input type="user" />
+            <Input type="user" value={userName} onChange={(e)=>setUserName(e.target.value)} />
           </FormControl>
 
           <FormControl id="password">
             <FormLabel>Senha</FormLabel>
-            <Input type="password" />
+            <Input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} />
           </FormControl>
-
-          <Button mt={5} mr={5} >
-            <LinkButton title="Login" route="/centralerrors"/>
+          <Text color="red">{message}</Text>
+          <Button mt={5} mr={5} onClick={() => handleLogin()}>
+            Login
           </Button>
           <Button mt={5}>
             <LinkButton title="Sign In" route="/register"/>
--- src/service/api.js
+++ src/service/api.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
-const api = axios. create({
+const api = axios.create({
     baseURL: "http://localhost:8080",
+    headers: {"Access-Control-Allow-Origin": "*"},
 });
 
-// const loginUser = (email, password) =>
-//   api.post("/user", { email, password });
+const loginUser = (email, password) =>
+  api.post("/oauth/token", { email, password });
 
 const registerUser = (name, email, password) => api.post('/user', {name, email, password});
 
-export default {registerUser};
+export default {registerUser, loginUser};
