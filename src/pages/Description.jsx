460333291baa291a73cc0a67cb2a0c50cd2b2bb0
jorgehsosiro <jojimurakabe@gmail.com>
2021-07-16 17:00:36 -0300

    Description chart & list

--- src/pages/Description.jsx
+++ src/pages/Description.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   CircularProgress,
   Tabs,
@@ -6,6 +6,8 @@ import {
   TabPanels,
   Tab,
   TabPanel,
+  FormLabel,
+  Input
 } from "@chakra-ui/react";
 import { Center, Box } from "@chakra-ui/layout";
 
@@ -20,10 +22,40 @@ export default function Description() {
   const [warningLevel, setWarningLevel] = useState([]);
   const [errorLevel, setErrorLevel] = useState([]);
   const [allData, setAllData] = useState([]);
+  const [page, setPage] = useState(0);
+  const [size, setSize] = useState(10);
+  const [desc, setDesc] = useState('CPF');
+
+  useEffect(() => {
+    api.getErrorBySubject('description',desc).then((response) => {
+      setAllData(response.data.content);
+      console.log(response);
+      response.data.content.map((item) => {
+        if (item.level === "INFO") {
+          return setInfoLevel(infoLevel.push(item));
+        } else if (item.level === "ERROR") {
+          return setErrorLevel(errorLevel.push(item));
+        } else {
+          return setWarningLevel(warningLevel.push(item));
+        }
+      });
+    });
+  }, []);
+
+
   return (
     <div>
       <Header title="Perfil" />
       <CentralErrorsSideBar />
+      <Box>
+      <FormLabel>Filtro</FormLabel>
+            <Input
+              type="text"
+              placeholder="2021-10-30"
+              value={desc}
+              onChange={(e) => setDesc(e.target.value)}
+            />
+      </Box>
       <Center>
         <Tabs isFitted variant="enclosed">
           <TabList mb="1em">
