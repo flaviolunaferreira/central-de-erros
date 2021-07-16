bcd77fc9b906ac79751ac195d95df823f1475d1f
jorgehsosiro <jojimurakabe@gmail.com>
2021-07-15 23:41:01 -0300

    add: toggle chart & List

--- src/components/ErrorList.jsx
+++ src/components/ErrorList.jsx
@@ -1,19 +1,32 @@
 import React from "react";
-import { Accordion } from "@chakra-ui/react";
-import ErrorLabel from "./ErrorLabel";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box
+} from "@chakra-ui/react";
 
 export default function ErrorList({ response }) {
   return (
     <Accordion allowMultiple>
-      {response.forEach(({ level, date, origin, description, log }) => {
-        <ErrorLabel
-          level={level}
-          date={date}
-          origin={origin}
-          description={description}
-          log={log}
-        />;
-      })}
+      {response.map(({ level, date, origin, description, log }) => (
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box flex="1" textAlign="left">
+                {level}-{date}-{origin}
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            <Box>Descrição:{description}</Box>
+            <Box>Log:{log}</Box>
+          </AccordionPanel>
+        </AccordionItem>
+      ))}
     </Accordion>
   );
 }
