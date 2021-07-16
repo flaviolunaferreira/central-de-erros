e4c063614cb831dcc40738afb1157561a312e40a
jorgehsosiro <jojimurakabe@gmail.com>
2021-07-16 16:23:24 -0300

    update branch & description chart begin

--- src/pages/Description.jsx
+++ src/pages/Description.jsx
@@ -1,13 +1,58 @@
-import React from 'react'
-import { CentralErrorsSideBar } from '../components/CentralErrorsSideBar'
-import Header from '../components/Header'
+import React, { useState } from "react";
+import {
+  CircularProgress,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+} from "@chakra-ui/react";
+import { Center, Box } from "@chakra-ui/layout";
+
+import { CentralErrorsSideBar } from "../components/CentralErrorsSideBar";
+import Header from "../components/Header";
+import Grafico from "../components/Grafico";
+import ErrorList from "../components/ErrorList";
+import api from "../service/errorApi";
 
 export default function Description() {
+  const [infoLevel, setInfoLevel] = useState([]);
+  const [warningLevel, setWarningLevel] = useState([]);
+  const [errorLevel, setErrorLevel] = useState([]);
+  const [allData, setAllData] = useState([]);
   return (
     <div>
       <Header title="Perfil" />
-      Busca por descrição
       <CentralErrorsSideBar />
+      <Center>
+        <Tabs isFitted variant="enclosed">
+          <TabList mb="1em">
+            <Tab>Grafico</Tab>
+            <Tab>Lista</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Center>
+                <Grafico
+                  firstLevel={infoLevel}
+                  secondLevel={warningLevel}
+                  thirdLevel={errorLevel}
+                  titulo="# de levels/niveis"
+                />
+              </Center>
+            </TabPanel>
+            <TabPanel>
+              <Box w="1000px">
+                {allData ? (
+                  <ErrorList response={allData} />
+                ) : (
+                  <CircularProgress isIndeterminate color="blue.300" />
+                )}
+              </Box>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Center>
     </div>
-  )
+  );
 }
--- src/service/errorApi.js
+++ src/service/errorApi.js
@@ -1,9 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
+
+const myToken = JSON.parse(localStorage.getItem("getToken"));
 
 const api = axios.create({
-    baseURL: "http://localhost:8080",
+  baseURL: "http://localhost:8080",
+  headers: {
+    Authorization: "Bearer " + myToken.access_token,
+  },
 });
 
 const getAllErrorLevel = () => api.get("api/0/50");
 
-export default {getAllErrorLevel};
+export default { getAllErrorLevel };
